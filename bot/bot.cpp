138a06f425081d4d007b35e71257aec5a19f2834
Ibrahim Algac <ialgac@k1m35s01.42istanbul.com.tr>
2023-07-28 00:52:59 +0300

    bot client

---
+++ bot/bot.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+# include <unistd.h>
+
+# include <fcntl.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
